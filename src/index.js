1f9d7b66153480e2abf829717b9ae4f3c1f9f2b7
hansadong <jhhan1108@gmail.com>
2024-09-17 01:11:27 +0900

    프로젝트 first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js';
+import './scripts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
